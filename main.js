824621b9294cdd18f1f7419e67b60a636e3de9c2
A. M. Hammond <monroe@Alexandrias-MacBook-Pro.local>
2019-02-19 19:58:22 -0500

    tc's lesson

---
+++ main.js
@@ -0,0 +1,22 @@
+$("body").on("click", ".dot", function (e) {
+	// on click add an active class to the dot that was clicked
+	e.preventDefault();
+
+	const $this_dot = $(this);
+
+	$this_dot.addClass("active");
+
+	//const = cannot change, let = can change //
+});
+
+$("body").on("click", ".dot .close", function (e) {
+	// on click add an active class to the dot that was clicked
+	e.preventDefault();
+	e.stopPropagation();
+
+	const $this_close = $(this);
+	const $this_dot = $this_close.closest(".dot");
+
+	$this_dot.removeClass("active")
+
+});
